0fa28de3440473345c3ab402040a056331bde075
haoling <haoling@corp.netease.com>
2025-01-15 10:39:36 +0800

    optimize: 将 mysql2 的数据库连接方式改为 sequelize 连接

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,28 @@
-import mysql from'mysql2/promise';
+import { Sequelize, DataTypes } from 'sequelize';
+import mysql2 from 'mysql2'
 
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'localhost',
-    database: 'Daily',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+// const pool = mysql.createPool({
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: 'localhost',
+//   database: 'Daily',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// })
 
-export default pool;
+export const sequelize = new Sequelize('Daily', 'root', 'localhost', {
+  host: 'localhost',
+  dialect: 'mysql',
+  dialectModule: mysql2,
+})
+
+export const RoutineTypeModal = sequelize.define('routine_type', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  type: DataTypes.STRING,
+  des: DataTypes.STRING,
+})
