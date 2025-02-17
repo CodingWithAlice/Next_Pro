0677c0873ceed89a71bd5a11e448f0c81b6fba62
CodingWithAlice <2269131254@qq.com>
2025-02-17 10:56:25 +0800

    feat: 二次阅读工具 保存的数据结构梳理

--- src/app/read/page.tsx
+++ src/app/read/page.tsx
@@ -2,6 +2,8 @@
 import { transTextArea, transTimeStringToType } from '@/components/tool';
 import './app.css';
 import { useState } from 'react';
+import { Button } from 'antd';
+import { AntDesignOutlined } from '@ant-design/icons';
 
 export default function ReadPage() {
     const [readData, setReadData] = useState<{ title?: string, firstTime?: string, secondTime?: string, plans?: string }>({
@@ -37,12 +39,27 @@ export default function ReadPage() {
         },
     ]);
 
-    const handleChange = (v: { [key: string]: string }) => {
-        setReadData({ ...readData, ...v });
+    const handleChange = (v: { [key: string]: string }, options?: { type: 'chapterData', sort: string }) => {
+        if (!options) {
+            setReadData({ ...readData, ...v });
+            return;
+        }
+        // 根据 options.sort 找到对应的章节数据，然后更新
+        setChapterData(chapterData.map((it) => {
+            if (+it.sort === parseInt(options.sort)) {
+                return { ...it, ...v };
+            }
+            return it;
+        }));
     }
-    const handleTrans = (it: { key: string, desc?: string }, source?: { [key: string]: string }) => {
+
+    const handleTrans = (it: { key: string, desc?: string }, source?: { [key: string]: string }, options?: { type: 'chapterData', sort: string }) => {
         if (!source) return;
-        return transTextArea({ ...it, source, onChange: handleChange });
+        return transTextArea({ ...it, source, onChange: (v) => handleChange(v, options) });
+    }
+
+    const handleSave = () => {
+        console.log(readData, chapterData);
     }
 
     return <div className='read'>
@@ -54,15 +71,21 @@ export default function ReadPage() {
                     { key: 'secondTime', desc: '重读时间' },
                 ].map(it => handleTrans(it, readData))}
                 {chapterData.map((chapter) => {
-                    return <>
-                        Chapter{chapter.sort}：
+                    return <div key={chapter.sort}>
+                        <p className='chapter-title'>Chapter{chapter.sort}：</p>
                         {[
                             { key: 'firstTimeTopic', desc: '第一次阅读重点' },
                             { key: 'secondTimeTopic', desc: '第二次阅读重点' },
                             { key: 'changes', desc: '变化/聚焦差异' },
-                        ].map(it => handleTrans(it, chapter))}
-                    </>
+                        ].map(it => handleTrans(it, chapter, {
+                            type: 'chapterData',
+                            sort: chapter.sort,
+                        }))}
+                    </div>
                 })}
+                <Button onClick={handleSave} icon={<AntDesignOutlined />}>
+                    保存
+                </Button>
             </ul>
         </nav>
 
