d314a14ba2d5715e6b1417a9408f850abec5b455
CodingWithAlice <2269131254@qq.com>
2025-02-19 15:11:06 +0800

    fix: 找不到 @/component 问题，alias 问题

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,29 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const tsconfigPath = path.resolve(__dirname, 'tsconfig.json')
+const tsconfigRaw = fs.readFileSync(tsconfigPath, 'utf-8')
+const tsconfig = JSON.parse(tsconfigRaw)
 
 // 在构建和运行时使用
 const nextConfig: NextConfig = {
-  /* config options here */
-  env: {
-    // API_HOST: process.env.NEXT_PUBLIC_API_HOST,
-  }
-};
+	webpack: (config) => {
+		const alias: { [key: string]: string } = {}
+		const paths = tsconfig.compilerOptions.paths
+		if (paths) {
+			Object.keys(paths).forEach((key) => {
+				const newKey = key.replace('/*', '')
+				const value = paths[key][0].replace('/*', '')
+				alias[newKey] = path.resolve(__dirname, value)
+			})
+		}
+		config.resolve.alias = {
+			...config.resolve.alias,
+			...alias,
+		}
+		return config
+	},
+}
 
-export default nextConfig;
+export default nextConfig
