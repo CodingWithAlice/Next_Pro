0348b76c6a7ce2c3ea85e7a35cb54b692691a3dd
CodingWithAlice <2269131254@qq.com>
2025-03-03 18:23:13 +0800

    fix: week/period 页面 form样式问题

--- src/app/components/week-day.tsx
+++ src/app/components/week-day.tsx
@@ -10,6 +10,55 @@ export interface WeekDayProps extends IssueRecordProps {
     date: string;
 }
 
+const items = [
+    {
+        label: "总计",
+        name: "total",
+    },
+    {
+        label: "LTN做题",
+        name: "ltnTotal",
+    },
+    {
+        label: "入睡时间",
+        name: "sleep",
+    },
+    {
+        label: "TED",
+        name: "tedShow",
+        render: <Switch disabled />
+    },
+    {
+        label: "阅读",
+        name: "read",
+        render: <Switch disabled />
+    },
+    {
+        label: "运动",
+        name: "sport",
+        render: <Switch disabled />
+    },
+    {
+        label: "电影",
+        name: "video",
+    },
+    {
+        label: "前端",
+        name: "front",
+        render: <Input.TextArea autoSize={{ minRows: 6, maxRows: 15 }} disabled />
+    },
+    {
+        label: "可以更好",
+        name: "better",
+        render: <Input.TextArea autoSize={{ minRows: 6, maxRows: 15 }} disabled />
+    },
+    {
+        label: "做得棒",
+        name: "good",
+        render: <Input.TextArea autoSize={{ minRows: 2, maxRows: 15 }} disabled />
+    },
+]
+
 function transformData(data: WeekDayProps) {
     const front = data.daily_time_records.find(item => item.routineTypeId === config.frontTotalId);
     const ltn = data.daily_time_records.find(item => item.routineTypeId === config.ltnTotalId);
@@ -28,65 +77,30 @@ function transformData(data: WeekDayProps) {
     }
 }
 
+
 export function WeekDay({ data, index }: { data: WeekDayProps, index: number }) {
     const [form] = Form.useForm();
     form.setFieldsValue(transformData(data));
-    const items = [
-        {
-            label: "总计",
-            name: "total",
-        },
-        {
-            label: "LTN做题",
-            name: "ltnTotal",
-        },
-        {
-            label: "入睡时间",
-            name: "sleep",
-        },
-        {
-            label: "TED",
-            name: "tedShow",
-            render: <Switch disabled />
-        },
-        {
-            label: "阅读",
-            name: "read",
-            render: <Switch disabled />
-        },
-        {
-            label: "运动",
-            name: "sport",
-            render: <Switch disabled />
-        },
-        {
-            label: "电影",
-            name: "video",
-        },
-        {
-            label: "前端",
-            name: "front",
-            render: <Input.TextArea autoSize={{ minRows: 6, maxRows: 15 }} disabled />
-        },
-        {
-            label: "可以更好",
-            name: "better",
-            render: <Input.TextArea autoSize={{ minRows: 6, maxRows: 15 }} disabled />
-        },
-        {
-            label: "做得棒",
-            name: "good",
-            render: <Input.TextArea autoSize={{ minRows: 2, maxRows: 15 }} disabled />
-        },
-    ]
+
+    const labelCol = {
+        xs: { span: 8 }, // 超小屏幕（手机）
+        sm: { span: 6 }, // 小屏幕（平板）
+        // 可以继续添加其他屏幕尺寸的配置
+      };
+      const wrapperCol = {
+        xs: { span: 16 },
+        sm: { span: 18 },
+        // 可以继续添加其他屏幕尺寸的配置
+      };
+    
     return <div className="week-day">
         <section>
             <h2>Day{index + 1} {data.date}</h2>
         </section>
-        <Form form={form} labelCol={{ span: 6 }} className="form">
+        <Form form={form} labelCol={labelCol} wrapperCol={wrapperCol}>
             {items.map(item =>
                 <Form.Item key={item.name} label={item.label} name={item.name}>
-                    {item?.render || <Input className="input" disabled />}
+                    {item?.render || <Input disabled />}
                 </Form.Item>)}
         </Form>
     </div>
