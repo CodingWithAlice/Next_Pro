9b51246d0d80c04799c839378165ad3d50049ead
CodingWithAlice <2269131254@qq.com>
2025-03-09 16:19:07 +0800

    fix: 移除 webpack 配置

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from 'next'
-import * as fs from 'fs'
 import * as path from 'path'
 import dotenv from 'dotenv'
 
@@ -7,26 +6,11 @@ import dotenv from 'dotenv'
 const configPath = path.resolve(__dirname, './config.env')
 dotenv.config({ path: configPath })
 
-const tsconfigPath = path.resolve(__dirname, 'tsconfig.json')
-const tsconfigRaw = fs.readFileSync(tsconfigPath, 'utf-8')
-const tsconfig = JSON.parse(tsconfigRaw)
 
 // 在构建和运行时使用
 const nextConfig: NextConfig = {
 	webpack: (config) => {
-		const alias: { [key: string]: string } = {}
-		const paths = tsconfig.compilerOptions.paths
-		if (paths) {
-			Object.keys(paths).forEach((key) => {
-				const newKey = key.replace('/*', '')
-				const value = paths[key][0].replace('/*', '')
-				alias[newKey] = path.resolve(__dirname, value)
-			})
-		}
-		config.resolve.alias = {
-			...config.resolve.alias,
-			...alias,
-		}
+		
 		return config
 	},
 	env: {
