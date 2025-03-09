9a828a3fa2775bf139055f7d89c0628284b64752
CodingWithAlice <2269131254@qq.com>
2025-03-09 18:19:05 +0800

    fix: 修改 error 提示

--- src/app/week/page.tsx
+++ src/app/week/page.tsx
@@ -23,7 +23,7 @@ export default function Week() {
     const handleSave = () => {
         const current = +curSerial === 0 ? serialsLength + 1 : curSerial;
         Api.postWeekApi({ ...weekData, serialNumber: current }).then((e) => {
-            messageApi.success(e.data.message);
+            messageApi.success(e?.data?.message || e?.message);
         }).catch((e) => {
             messageApi.error(e.message || '保存失败');
         })
@@ -33,7 +33,7 @@ export default function Week() {
         Api.getWeekApi(curSerial).then(({ weekData, serialData }) => {
             const currentSerial = serialData.filter((it: { [key: string]: string }) => +it.serialNumber === curSerial)?.[0];
             const gap = getGapTime(currentSerial?.startTime, currentSerial?.endTime, 'day');
-            const time = currentSerial ? `${currentSerial?.startTime} 至 ${currentSerial?.endTime} ${gap}天` : '新周期';
+            const time = currentSerial ? `${currentSerial?.startTime} 至 ${currentSerial?.endTime} ${gap + 1}天` : '新周期';
 
             setWeekData({ ...weekData, time });
         })
