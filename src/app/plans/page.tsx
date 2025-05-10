9a20984aaa10135e70b83988db81216df20b2b74
CodingWithAlice <2269131254@qq.com>
2025-05-10 19:29:32 +0800

    feat: 修改‘二次阅读’为‘知行录’，记录 TED、阅读/电影、运动

---
+++ src/app/plans/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { transTextArea, transTimeStringToType } from '@/components/tool';
+import './app.css';
+import { useEffect, useState } from 'react';
+import { Button, message, Space } from 'antd';
+import { AntDesignOutlined } from '@ant-design/icons';
+import Api from '@/service/api';
+
+
+export default function PlanPage() {
+    
+    
+    return <div className='outer plan'>
+        111
+    </div>
+}
